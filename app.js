9c51a94bcf1146e041371e01a23c713bf4f993f4
DanielVierh <DanielVierh@users.noreply.github.com>
2023-02-19 19:01:43 +0100

    added select field for eatingtime

--- app.js
+++ app.js
@@ -1,53 +1,57 @@
 // Projekt erstellt am 13.05.2021
 
 // Variabeln
-let buttonAdd = document.getElementById('btnAdd');
-let buttonScroll_Up = document.getElementById('btnscrl_Up');
-var haferflocken = '';
-var today_Steps = 0;
-var array_Food_DB = [];
-var today_eaten = [];
-var my_Statistics = [];
-var additional_Targets = [];
-var my_History = [];
-var bodyWeight = 78;
-var kcal_Ziel = 2000;
-var kcal_Requirement = 2000;
+const buttonAdd = document.getElementById('btnAdd');
+const buttonScroll_Up = document.getElementById('btnscrl_Up');
+let haferflocken = '';
+let today_Steps = 0;
+let array_Food_DB = [];
+let today_eaten = [];
+let my_Statistics = [];
+let additional_Targets = [];
+let my_History = [];
+let bodyWeight = 78;
+let kcal_Ziel = 2000;
+let kcal_Requirement = 2000;
 
-var selected_Food = '';
-var selectedFoodIndex = 0;
-var foodFromToday = false;
+let selected_Food = '';
+let selectedFoodIndex = 0;
+let foodFromToday = false;
 
-var eaten_Kcal = 0;
-var eaten_Carbs = 0.0;
-var eaten_Sugar = 0.0;
-var eaten_Protein = 0.0;
-var eaten_Fat = 0.0;
-var eaten_Salt = 0.0;
-var eaten_Fiber = 0.0;
-var eaten_Amount = 0.0;
-var burned_Kcal = 0;
-var effective_Kcal = 0;
-var form_is_Invisible = false;
-var diff = 0;
-var changeProduct = false;
-var old_Quantity = 0;
-var max_Sugar = 50;
-var max_Salt = 7;
-var min_Protein = 90;
-var min_Fiber = 30;
-var min_Steps = 10000;
-var des_Fat = 80;
-var des_Carbs = 150;
-var new_Water = 0.25;
-var today_Water = 0;
-var originDB = [];
-var exp_New_Prod = [];
-var selectedDateIndex = 0;
-var selectedDate = '';
-var lastWater = '';
-var spezDiet_Visible = false;
-var isKeto = false;
+let eaten_Kcal = 0;
+let eaten_Carbs = 0.0;
+let eaten_Sugar = 0.0;
+let eaten_Protein = 0.0;
+let eaten_Fat = 0.0;
+let eaten_Salt = 0.0;
+let eaten_Fiber = 0.0;
+let eaten_Amount = 0.0;
+let burned_Kcal = 0;
+let effective_Kcal = 0;
+let form_is_Invisible = false;
+let diff = 0;
+let changeProduct = false;
+let old_Quantity = 0;
+let max_Sugar = 50;
+let max_Salt = 7;
+let min_Protein = 90;
+let min_Fiber = 30;
+let min_Steps = 10000;
+let des_Fat = 80;
+let des_Carbs = 150;
+let new_Water = 0.25;
+let today_Water = 0;
+let originDB = [];
+let exp_New_Prod = [];
+let selectedDateIndex = 0;
+let selectedDate = '';
+let lastWater = '';
+let spezDiet_Visible = false;
+let isKeto = false;
+let breakfast = [];
+let lunch = [];
+let dinner = [];
+let currentMealTime = 'br'
 
 let countedPercentNumber = 0;
 let originalPercentValue = 0;
@@ -73,12 +77,13 @@ const outp_nutriScore = document.getElementById("outp_nutriScore");
 const modalOverlay = document.getElementById('modalOverlay');
 const btn_ShowModalButton = document.getElementById('btn_ShowModal');
 const btn_CloseModal = document.getElementById('close-modal');
+const select_EatTime = document.getElementById("sel_Eattime");
 
 // Modal einblenden
 btn_ShowModalButton.addEventListener('click', () => {
     body.classList.add("notAv");
     modalOverlay.style.display = 'block';
-    console.log(`Bodyheight: ${body.style.height}`);
+    console.log('currentMealTime', currentMealTime);
 });
 
 
@@ -145,7 +150,6 @@ function toggleFullScreen() {
         document.exitFullscreen();
     } else {
         document.documentElement.requestFullscreen().catch((error) => {
-            //console.log(error);
         });
     }
 }
@@ -1094,6 +1098,8 @@ function closeModal(modal) {
     overlay.classList.remove('active');
 }
 
+
+
 function get_new_Steps() {
     try {
         today_Steps = parseInt(document.getElementById('inp_Steps').value);
@@ -1492,7 +1498,7 @@ function averageNutriScore() {
                 outp_nutriScore.style.color = 'white';
                 break;
             case 2:
-                outp_nutriScore.innerHTML = 'B'; 
+                outp_nutriScore.innerHTML = 'B';
                 outp_nutriScore.style.backgroundColor = 'lightgreen';
                 outp_nutriScore.style.color = 'black';
                 break;
@@ -1609,6 +1615,28 @@ function checkButton() {
 //============================================================================
 // Food zu heute gegessen hinzufügen
 //============================================================================
+
+function changeEatTime(selectObject) {
+    const value = selectObject.value;
+    setEatTime(value);
+}
+
+function setEatTime(value) {
+    if (value === 'opt_normal') {
+        console.log('Breakfast');
+        currentMealTime = 'br';
+    }
+    if (value === 'lu') {
+        console.log('Lunch');
+        currentMealTime = 'lu';
+    }
+    if (value === 'di') {
+        console.log('Dinner');
+        currentMealTime = 'di';
+    }
+}
+
+
 function add_Food_to_TodayList() {
     //Produkt nicht "", also ausgewählt
     if (selected_Food != '') {
@@ -1616,24 +1644,29 @@ function add_Food_to_TodayList() {
             alert('Bitte eine Menge eingeben');
         } else {
             let newProduct = selected_Food.productName;
-            var alreadyTracked = false;
-            var todayEatenIndex = 3000;
-            var selectedAmount = parseFloat(
+            let alreadyTracked = false;
+            let todayEatenIndex = -1;
+            let selectedAmount = parseFloat(
                 inputField_EatenFood_in_Gramm.value,
             );
 
             // Checke ob bereits vorhanden
-            for (var i = 0; i < today_eaten.length; i++) {
+            for (let i = 0; i < today_eaten.length; i++) {
                 if (today_eaten[i].intake_productName == newProduct) {
                     alreadyTracked = true;
                     todayEatenIndex = i;
                     break;
                 }
             }
-            if (alreadyTracked == false) {
-            } else {
+            // TODO - Zusätzlich Checken, ob in den unterArrays Breakfast und Dinner etc vorhanden
+            if(checkEatTimeArrays(newProduct).eaten === true) {
+                console.log('Unterarray betroffen');
+            }
+
+
+            if (alreadyTracked === true) {
                 // Fragen, ob addiert werden soll
-                var addRequest = window.confirm(
+                const addRequest = window.confirm(
                     newProduct +
                     ' ist bereits in Deiner Liste vorhanden. Soll der Wert dazu addiert werden?',
                 );
@@ -1645,11 +1678,11 @@ function add_Food_to_TodayList() {
                     // Neuen Wert eintragen alt + neu
                     selectedAmount = selectedAmount + old_Quantity;
                     // Altes Produkt löschen
-                    if (todayEatenIndex != 3000) {
+                    if (todayEatenIndex != -1) {
                         today_eaten.splice(todayEatenIndex, 1);
                     }
                     // Letzte Aktionen
-                    todayEatenIndex = 3000;
+                    todayEatenIndex = -1;
                 } else {
                     createTable_FoodDB();
                     blendOut_Eingabebereich_FoodDB();
@@ -1695,6 +1728,56 @@ function add_Food_to_TodayList() {
                     ),
                 );
 
+                if(currentMealTime === 'br') {
+                    breakfast.push(
+                        new TodayEatenFood(
+                            newProduct,
+                            selectedAmount,
+                            kcal_Intake,
+                            fat_Intake,
+                            carb_Intake,
+                            sugar_Intake,
+                            protein_Intake,
+                            salt_Intake,
+                            fiber_Intake,
+                        ),
+                    )
+                }
+                if(currentMealTime === 'lu') {
+                    breakfast.push(
+                        new TodayEatenFood(
+                            newProduct,
+                            selectedAmount,
+                            kcal_Intake,
+                            fat_Intake,
+                            carb_Intake,
+                            sugar_Intake,
+                            protein_Intake,
+                            salt_Intake,
+                            fiber_Intake,
+                        ),
+                    )
+                }
+                if(currentMealTime === 'di') {
+                    breakfast.push(
+                        new TodayEatenFood(
+                            newProduct,
+                            selectedAmount,
+                            kcal_Intake,
+                            fat_Intake,
+                            carb_Intake,
+                            sugar_Intake,
+                            protein_Intake,
+                            salt_Intake,
+                            fiber_Intake,
+                        ),
+                    )
+                }
+
+                console.log('Breakfast', breakfast);
+                console.log('lunch', lunch);
+                console.log('dinner', dinner);
+
                 // Anzeigen, dass Produkt eingetragen wurde
                 let intakeFoodInfo =
                     newProduct +
@@ -1735,6 +1818,45 @@ function add_Food_to_TodayList() {
     }
     create_Table_TodayEaten();
     calc_Values();
+}
+
+function checkEatTimeArrays(newProduct) {
+    let returnObj = {
+        eaten: false,
+        arr: ''
+    }
+
+    if(currentMealTime === 'br') {
+        for (let i = 0; i < breakfast.length; i++) {
+            if (breakfast[i].intake_productName == newProduct) {
+                returnObj.eaten = true;
+                returnObj.arr = 'breakfast'
+                break
+            }
+        }
+    }
+
+    if(currentMealTime === 'lu') {
+        for (let i = 0; i < lunch.length; i++) {
+            if (lunch[i].intake_productName == newProduct) {
+                returnObj.eaten = true;
+                returnObj.arr = 'lunch'
+                break
+            }
+        }
+    }
+
+    if(currentMealTime === 'di') {
+        for (let i = 0; i < dinner.length; i++) {
+            if (dinner[i].intake_productName == newProduct) {
+                returnObj.eaten = true;
+                returnObj.arr = 'dinner'
+                break
+            }
+        }
+    }
+
+    return returnObj;
 }
 
 //============================================================================
